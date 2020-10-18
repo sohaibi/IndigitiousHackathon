124e715ffad78dcc016c51d9e2f685de1813c30b
sohaibi <sohaibkhann97@gmail.com>
2020-10-17 22:26:22 -0500

    Pushing this awesome project
    Good Luck

---
+++ volunteering.js
@@ -0,0 +1,31 @@
+var subjectObject = {
+    "9:00": {      
+    },
+    "9:30": {
+    },
+    "10:00": {
+    },
+    "10:30": {
+    }
+  }
+  window.onload = function() {
+    var subjectSel = document.getElementById("subject");
+    var topicSel = document.getElementById("topic");
+    var chapterSel = document.getElementById("chapter");
+    for (var x in subjectObject) {
+      subjectSel.options[subjectSel.options.length] = new Option(x, x);
+    }
+    subjectSel.onchange = function() {
+      //display correct values
+      for (var y in subjectObject[this.value]) {
+        topicSel.options[topicSel.options.length] = new Option(y, y);
+      }
+    }
+    topicSel.onchange = function() {
+      //display correct values
+      var z = subjectObject[subjectSel.value][this.value];
+      for (var i = 0; i < z.length; i++) {
+        chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
+      }
+    }
+  }
